33a823befe4623f3b98dea0ef524353d7a5ead31
Olav <osmevoll@gmail.com>
2021-04-12 15:38:39 +0200

    finished js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,13 @@
 const raceTrack = document.getElementById("race-track");
+const laneNumber = document.getElementById("lane-numbers");
+
 let finished = 0;
+
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+    addLanes(4);
+});
 
 document.getElementById("numbrCars").addEventListener("change", (event) =>
 {
@@ -9,15 +17,16 @@ document.getElementById("numbrCars").addEventListener("change", (event) =>
 document.getElementById("startBtn").addEventListener("click", () =>
 {
     let cars = document.querySelectorAll(".car");
+    let lanes = document.querySelectorAll(".lane-numb");
     let speedNumbers = getRandomNumbersArray(cars.length);
-    let trackWidth = raceTrack.offsetWidth - 55;
+    let trackWidth = raceTrack.offsetWidth - 65;
 
     for (let i = 0; i < cars.length; i++)
     {
         cars[i].style.transition = `transform ${speedNumbers[i]}s cubic-bezier(.${getRandomNumber()},.${getRandomNumber()},.${getRandomNumber()},.${getRandomNumber()})`;
         cars[i].style.transform = `translate(${trackWidth}px)`;
     }
-    announcePlacements(cars);
+    announcePlacements(cars, lanes);
 });
 
 document.getElementById("resetBtn").addEventListener("click", () =>
@@ -28,8 +37,8 @@ document.getElementById("resetBtn").addEventListener("click", () =>
 const getRandomNumbersArray = (cars) => 
 {
     let speedArray = [];
-    const min = 2;
-    const max = 11;
+    const min = 5;
+    const max = 16;
 
     while (speedArray.length < cars)
     {
@@ -56,6 +65,14 @@ const addLanes = (lanes) =>
     {
         let raceLane = document.createElement("div");
         raceLane.classList.add("race-lane");
+        let lane = document.createElement("div");
+        lane.classList.add("lane-numb");
+        lane.textContent = i + 1;
+        if(i % 2 === 0)
+        {
+            raceLane.style.background = "#8ee4af";
+            lane.style.background = "#8ee4af";
+        }
         let car = document.createElement("div");
         let carIcon = document.createElement("img");
         carIcon.setAttribute("src", "../images/carImg.svg");
@@ -63,6 +80,8 @@ const addLanes = (lanes) =>
         car.appendChild(carIcon);
         raceLane.appendChild(car);
         raceTrack.appendChild(raceLane);
+        laneNumber.appendChild(lane);
+
     }
 }
 
@@ -71,27 +90,31 @@ const removeLanes = () =>
     while (raceTrack.firstChild)
     {
         raceTrack.removeChild(raceTrack.lastChild);
+        laneNumber.removeChild(laneNumber.lastChild);
     }
 }
 
-const announcePlacements = (cars) =>
+const announcePlacements = (cars, lanes) =>
 {
     for (let i = 0; i < cars.length; i++)
     {
-        let lane = cars[i].parentElement;
+        let track = cars[i].parentElement;
         cars[i].addEventListener('transitionend', () =>
         {
             if(finished === 0)
             {
-                lane.style.background = "#FFD700";
+                track.style.background = "#FFD700";
+                lanes[i].style.background = "#FFD700";
             }
             else if(finished === 1)
             {
-                lane.style.background = "#B6C2CC";
+                track.style.background = "#B6C2CC";
+                lanes[i].style.background = "#B6C2CC";
             }
             else if(finished === 2)
             {
-                lane.style.background = "#cd7f32 ";
+                track.style.background = "#cd7f32";
+                lanes[i].style.background = "#cd7f32";
             }
             finished ++;            
         });
